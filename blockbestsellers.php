185c51c98ce7bfc2af6946594568a9d6d9f51cbf
Petr Hučík <petr@getdatakick.com>
2023-09-18 11:05:46 +0200

    Release blockbestsellers v2.2.5

--- blockbestsellers.php
+++ blockbestsellers.php
@@ -47,7 +47,7 @@ class BlockBestSellers extends Module
     {
         $this->name = 'blockbestsellers';
         $this->tab = 'front_office_features';
-        $this->version = '2.2.4';
+        $this->version = '2.2.5';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
